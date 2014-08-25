484ac933d9a93e23720ff07e383b61431850c45e
Narog <gmandini@net.hr>
2014-08-25 10:51:38 +0200

    'Storefront'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
 
-validates :title, :description, :image_url, :presence => true
-validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-validates :title, :uniqueness => true
+  default_scope :order => 'title'
 
-validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true, length: { in: 6..50 ,
+					message: "%{value} nije dulji od 6 a manji od 20, nista od tog borac!"}
+
+  validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
 	:message => 'must be a URL for GIF, JPG or PNG image.',:multiline => true}
 end
