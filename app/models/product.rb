ca115fec915fe944928aacf493ee507b106e3b7b
Narog <gmandini@net.hr>
2014-09-03 16:19:42 +0200

    cart ajaxed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,26 @@
 class Product < ActiveRecord::Base
 
   default_scope :order => 'title'
-
+  has_many :line_items
+  
+  
+  # A hook method is a method that
+  # Rails calls automatically at a given point in an object’s life. In this case, the
+  # method will be called before Rails attempts to destroy a row in the database.
+  # If the hook method returns false, the row will not be destroyed
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  # ensure that there are no line items referencing this product, when deleting product
+  def ensure_not_referenced_by_any_line_item  
+    if line_items.count.zero?
+     return true
+    else
+     errors[:base] << "Line Items present!"
+    return false
+    end
+  end
+  
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true, length: { in: 6..50 ,
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821161118) do
+ActiveRecord::Schema.define(version: 20140902132857) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.string   "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.string   "decimal"
     t.datetime "created_at"
     t.datetime "updated_at"
