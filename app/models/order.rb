edd30d596d8dcae6643969e0876bdf2b4ccaa2e9
Narog <gmandini@net.hr>
2014-09-08 13:53:25 +0200

    order details show

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
+  has_many :products, :through => :line_items
   
   PAYMENT_TYPES = [ "Check" , "Credit card" , "Purchase order" ]
   validates :name, :address, :email, :pay_type, :presence => true
